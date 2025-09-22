1718af1b75a132f7873408c1de0c98188dfb7415
Ka Ming Chan <52784641+kaming-chan@users.noreply.github.com>
2025-09-22 16:59:01 +0100

    Update code.js

--- code.js
+++ code.js
@@ -1,4 +1,4 @@
-define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/stiat/qualtrics/qstiat6.js'], function(APIConstructor, stiatExtension){
+define(['pipAPI','pipScorer','underscore'], function(APIConstructor, Scorer, _) {
 
 	function stiatExtension(options)
 	{
